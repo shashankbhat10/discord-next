e9bfe0d478fd094530b1559c7daba652f1f94760
Shashank Bhat <s.2205.b@gmail.com>
2024-01-10 01:05:55 -0500

    add theme toggle component

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/providers/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl='/' />
+      <ModeToggle />
     </div>
   );
 }
