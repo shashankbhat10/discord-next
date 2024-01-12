613d9c0bdc57c2f8c2645325366f4e48b3421c8c
Shashank Bhat <s.2205.b@gmail.com>
2024-01-12 03:54:13 -0500

    fix edit/delete event propogation. redirect to chat

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,7 +5,7 @@ import { Channel, Server, MemberRole, ChannelType } from "@prisma/client";
 import { Hash, Video, Mic, Edit, Trash, Lock } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "../action-tooltip";
-import { useModal } from "@/hooks/use-modal-store";
+import { ModalType, useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -26,8 +26,18 @@ export const ServerChannel = ({ channel, server, role }: ServerChannelProps) => 
 
   const Icon = iconMap[channel.type];
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { server, channel });
+  };
+
   return (
     <button
+      onClick={() => onClick()}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 \
         transition mb-1",
@@ -46,14 +56,14 @@ export const ServerChannel = ({ channel, server, role }: ServerChannelProps) => 
         <div className='ml-auto flex items-center gap-x-2'>
           <ActionTooltip label='Edit'>
             <Edit
-              onClick={() => onOpen("editChannel", { channel, server })}
+              onClick={(e) => onAction(e, "editChannel")}
               className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 
             dark:text-zinc-400 dark:hover:text-zinc-300 transition'
             />
           </ActionTooltip>
           <ActionTooltip label='Delete'>
             <Trash
-              onClick={() => onOpen("deleteChannel", { channel, server })}
+              onClick={(e) => onAction(e, "deleteChannel")}
               className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 
             dark:text-zinc-400 dark:hover:text-zinc-300 transition'
             />
