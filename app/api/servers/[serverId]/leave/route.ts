988ab88d0c00956bf8b75a33efa4734cbbe07424
Shashank Bhat <s.2205.b@gmail.com>
2024-01-11 23:14:05 -0500

    api to leave server

---
+++ app/api/servers/[serverId]/leave/route.ts
@@ -0,0 +1,25 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
+  try {
+    if (!params.serverId) return new NextResponse("serverId is missing", { status: 400 });
+    const profile = await currentProfile();
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: { not: profile.id },
+        members: { some: { profileId: profile.id } },
+      },
+      data: { members: { deleteMany: { profileId: profile.id } } },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_LEAVE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
