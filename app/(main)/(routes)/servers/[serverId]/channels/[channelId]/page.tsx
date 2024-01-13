02cc1d263d2a98129b948c3f41ecf87298387a96
Shashank Bhat <s.2205.b@gmail.com>
2024-01-13 01:32:37 -0500

    add messages component to page and fix apiUrl typo

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -30,11 +31,21 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
       <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
-      <div className='flex-1'>Chat Messages</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        type='channel'
+        apiUrl='/api/messages'
+        socketUrl='/api/socket/messages'
+        socketQuery={{ channelId: channel.id, serverId: channel.serverId }}
+        paramKey='channelId'
+        paramValue={channel.id}
+        chatId={channel.id}
+      />
       <ChatInput
         name={channel.name}
         type='channel'
-        apiUrl='/api/socket/message'
+        apiUrl='/api/socket/messages'
         query={{ channelId: channel.id, serverId: channel.serverId }}
       />
     </div>
