666f2f06d69fe69e9dc4edc23b7314bb4189ade9
Shashank Bhat <s.2205.b@gmail.com>
2024-01-13 02:40:22 -0500

    modal to delete messages

--- components/modals/delete-message-modal.tsx
+++ components/modals/delete-message-modal.tsx
@@ -5,27 +5,23 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "../ui/button";
 import { useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import qs from "query-string";
 
 export const DeleteMessageModal = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { isOpen, onClose, type, data } = useModal((state) => state);
-  const router = useRouter();
 
   const isModalOpen = isOpen && type === "deleteMessage";
-  const { server, channel } = data;
+  const { apiUrl, query } = data;
 
   const onClick = async () => {
     try {
       setIsLoading(true);
 
-      const url = qs.stringifyUrl({ url: `/api/channels/${channel?.id}`, query: { serverId: server?.id } });
+      const url = qs.stringifyUrl({ url: apiUrl || "", query });
       await axios.delete(url);
 
       onClose();
-      router.refresh();
-      router.push(`/servers/${server?.id}`);
     } catch (error) {
       console.log(error);
     } finally {
@@ -37,10 +33,8 @@ export const DeleteMessageModal = () => {
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className='bg-white text-black p-0 overflow-hidden'>
         <DialogHeader className='pt-8 px-6'>
-          <DialogTitle className='text-2xl text-center font-bold'>Delete Channel!</DialogTitle>
-          <DialogDescription className='text-center text-zinc-500'>
-            Are you sure you want to delete <span className='font-semibold text-indigo-500'>#{channel?.name}</span>
-          </DialogDescription>
+          <DialogTitle className='text-2xl text-center font-bold'>Delete Message!</DialogTitle>
+          <DialogDescription className='text-center text-zinc-500'>Are you sure you want to delete?</DialogDescription>
         </DialogHeader>
         <DialogFooter className='bg-gray-100 px-6 py-4'>
           <div className='flex items-center justify-between w-full'>
