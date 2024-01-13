9672844abddf110c17340d6af3eec9479e7f4779
Shashank Bhat <s.2205.b@gmail.com>
2024-01-12 21:24:33 -0500

    add chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -29,6 +30,13 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
       <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
+      <div className='flex-1'>Chat Messages</div>
+      <ChatInput
+        name={channel.name}
+        type='channel'
+        apiUrl='/api/socket/message'
+        query={{ channelId: channel.id, serverId: channel.serverId }}
+      />
     </div>
   );
 };
