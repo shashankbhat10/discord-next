81a24c3b550dc8687571f9a6c882260a1ad0d718
Shashank Bhat <s.2205.b@gmail.com>
2024-01-12 22:53:03 -0500

    open modal on button click in chat input

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -8,6 +8,7 @@ import { Plus, Smile } from "lucide-react";
 import { Input } from "../ui/input";
 import qs from "query-string";
 import axios from "axios";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -21,6 +22,8 @@ const formSchema = z.object({
 });
 
 export const ChatInput = ({ apiUrl, name, query, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     defaultValues: {
       content: "",
@@ -50,6 +53,7 @@ export const ChatInput = ({ apiUrl, name, query, type }: ChatInputProps) => {
               <FormControl>
                 <div className='relative p-4 pb-6'>
                   <button
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     type='button'
                     className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 
                     dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center'>
