47200247b5eeebae31d7a993853df2d50ee0968b
Shashank Bhat <s.2205.b@gmail.com>
2024-01-13 01:31:34 -0500

    chat messages component

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { Member, Message, Profile } from "@prisma/client";
+import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
+import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+import { format } from "date-fns";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
+
+interface ChatMessagesProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
+}
+
+type MessageWithMemberAndProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
+};
+
+export const ChatMessages = ({
+  apiUrl,
+  chatId,
+  member,
+  name,
+  paramKey,
+  paramValue,
+  socketQuery,
+  socketUrl,
+  type,
+}: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useChatQuery({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+  });
+
+  if (status === "pending") {
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <Loader2 className='animate-spin h-7 w-7 text-zinc-500 my-4' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>Loading...</p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <ServerCrash className='h-7 w-7 text-zinc-500 my-4' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>Something went wrong</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className='flex-1 flex flex-col py-4 overflow-y-auto'>
+      <div className='flex-1' />
+      <ChatWelcome type={type} name={name} />
+      <div className='flex flex-col-reverse mt-auto'>
+        {data?.pages?.map((group, i) => (
+          <Fragment key={i}>
+            {group.items.map((message: MessageWithMemberAndProfile) => (
+              <ChatItem
+                key={message.id}
+                currentMember={member}
+                member={message.member}
+                id={message.id}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
+            ))}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
