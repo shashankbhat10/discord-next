a358ef3a49124955b7a28df245016cde7ae3933d
Shashank Bhat <s.2205.b@gmail.com>
2024-01-12 23:25:18 -0500

    fix import after file name change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
-import { SocketProvider } from "@/components/providers/socket.provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
