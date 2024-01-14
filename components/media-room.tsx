0c83ae9641260cdea20b4ce3ebc42426152640de
Shashank Bhat <s.2205.b@gmail.com>
2024-01-13 19:42:11 -0500

    add livekit video/audio chat

---
+++ components/media-room.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface MediaRoomProps {
+  chatId: string;
+  video: boolean;
+  audio: boolean;
+}
+
+export const MediaRoom = ({ audio, chatId, video }: MediaRoomProps) => {
+  const { user } = useUser();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (!user?.firstName || !user?.lastName) return;
+
+    const name = `${user.firstName} ${user.lastName}`;
+
+    (async () => {
+      try {
+        const res = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
+        const data = await res.json();
+        setToken(data.token);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [user?.firstName, user?.lastName, chatId]);
+
+  if (token === "")
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <Loader2 className='h-7 w-7 animate-spin my-4 text-zinc-400' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>Loading...</p>
+      </div>
+    );
+
+  return (
+    <LiveKitRoom
+      data-lk-there='default'
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      token={token}
+      connect={true}
+      video={video}
+      audio={audio}>
+      <VideoConference />
+    </LiveKitRoom>
+  );
+};
